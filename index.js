ed431864adb3399b3fb24fc11e34925ac4cd797b
Allanshikomere <allanshikomere@gmail.com>
2023-12-20 21:03:09 +0300

    UsingArrayReduce

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteries = [5, 7, 8, 11];
+
+
+function calculateTotalBatteries(arr) {
+  return arr.reduce((acc, val) => acc + val, 0);
+}
+
+const totalBatteries = calculateTotalBatteries(batteries);
+
+
